3e2f09a13e19caaa97e453e3f64f777aa4129b5c
ShamserulAwsGeek <shamserul786@gmail.com>
2025-02-14 21:38:12 +0530

    variable file added

---
+++ variable.py
@@ -0,0 +1,45 @@
+dog_age = 49 / 7
+
+print(f"""Otto's dog age is {49/7}. So a dog that's about
+{49/7} would be the same age as Otto. Any dog born about {49/7}
+years ago would be in the same stage of life as Otto.""")
+
+print(f"""Otto's dog age is {dog_age}. So a dog that's about
+{dog_age} would be the same age as Otto. Any dog born about {dog_age}
+years ago would be in the same stage of life as Otto.""")
+
+print(f"""Otto's dog age is {dog_age:.2f}. So a dog that's about
+{dog_age} would be the same age as Otto. Any dog born about {dog_age}
+years ago would be in the same stage of life as Otto.""")
+
+print(f"""{name}'s dog age is {dog_age}. So a dog that's about
+{dog_age} would be the same age as {name}. Any dog born about {dog_age}
+years ago would be in the same stage of life as {name}.""")
+
+
+name = "Otto Matic"
+
+print(f"""{name}'s dog age is {dog_age}. So a dog that's about
+{dog_age} would be the same age as {name}. Any dog born about {dog_age}
+years ago would be in the same stage of life as {name}.""")
+
+
+# Create a variable called 'my_name' and assign it the value of your name as a string.
+# Then print out a greeting using the variable, like "Hello, Andrew!"
+my_name = "Hello, Sham"
+print(my_name)
+
+# Enter your favorite number below and store it in a variable called 'fav_num'. 
+# Print out a message telling you what your favorite number plus 10 is.
+fav_num = fav_num + 100
+print(f"Your favorite number plus 10 is {fav_num}")
+
+# Create two variables, 'countries_visited' and 'countries_to_visit' and assign them the number of
+# countries you've been to and the number of countries you hope to visit. Then complete the print statement.
+
+countries_visited= "India,Nepal,Bhutan"
+countries_to_visit= "Us, UK,UAE"
+num_of_countries_visited= 3
+
+print(f"""I have visited {countries_visited} countries. I plan to visit {countries_to_visit} more countries, 
+      and when I'm done I will have visited {num_of_countries_visited} countries.""")
