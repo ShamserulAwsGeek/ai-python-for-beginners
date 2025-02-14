64e9d70c870508509f6fed0c357418e487a0757c
ShamserulAwsGeek <shamserul786@gmail.com>
2025-02-14 21:08:27 +0530

    added data file

---
+++ data.py
@@ -0,0 +1,28 @@
+print(f"{((75 - 32) * 5 / 9):.2f}")
+
+print(f"The temperature 75F in degrees celsius is {(75 - 32) * 5 / 9}C")
+
+print(f"Isabel is {28/7:.0f} dog years old.")
+
+print(f"""
+    Most countries use the metric system for recipe measurement, 
+    but American bakers use a different system. For example, they use 
+    fluid ounces to measure liquids instead of milliliters (ml).
+    
+    So you need to convert recipe units to your local measuring system!
+    
+    For example, 8 fluid ounces of milk is {8 * 29.5735} ml.
+    And 100ml of water is {100 / 29.5735} fluid ounces.
+""")
+
+# Modify the code to print your age
+print(f"I am {29} years old.")
+
+# Fix this code
+print(f"There are {365/7} weeks in a year")
+
+# Complete the code
+print(f"The area of a square with side 5 cm is {5**2} cm squared.")
+
+# Modify the code to display one decimal place
+print(f"The house was a good size: 1200 square feet, or {1200 * 0.092903:.1f} meters squared!")
