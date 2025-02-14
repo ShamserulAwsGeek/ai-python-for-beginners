d187b03263c9e2ff8bbf02633e242d23b7892096
ShamserulAwsGeek <shamserul786@gmail.com>
2025-02-14 18:47:59 +0530

    added ai labs for python

---
+++ ai-labs.py
@@ -0,0 +1,18 @@
+# This is an example of code with multiple
+# comments, including this one which
+# spans multiple lines
+print("Hello, Shamserul_Maheen!")
+# Follow up with an additional print command
+print("How is your day going?")
+
+
+# Write code that displays your favorite color:
+print("my favourite color is Sapphire BlueS: Sapphire Blue")
+
+# Write code that answers the question "How are you feeling today?":
+
+# Ask the user how they are feeling
+feeling = input("How are you feeling today? ")
+
+# Respond with the user's input
+print("I'm glad to hear you're feeling", feeling + "!")
