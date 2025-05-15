f9f45c1d745d021fe6c7549e212c9bc59b3b892c
Md Shamserul Haque <shamserul786@gmail.com>
2025-05-15 22:33:45 +0530

    Update llm-prompts.py

--- llm-prompts.py
+++ llm-prompts.py
@@ -3,7 +3,7 @@ from helper_functions import print_llm_response #type: ignore
 print_llm_response("What is the capital of France?")
 
 name = "Otto Matic"
-dog_age = 22/7
+dog_age = 23/7
 
 print_llm_response(f"""If {name} were a dog, he would be {dog_age} years old.
 Describe what life stage that would be for a dog and what that might 
