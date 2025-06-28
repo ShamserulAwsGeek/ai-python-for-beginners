04220835bb83d85c3531693a764b9327b862e1e6
ShamserulAwsGeek <shamserul786@gmail.com>
2025-06-28 12:55:34 +0530

    python lab

---
+++ weather.py
@@ -0,0 +1,22 @@
+tempreture = 95
+if tempreture > 80:
+    print("its too hot!")
+    print("stay inside")
+print("Enjoy your day with a cup of tea & beautiful wife")
+
+
+tempreture = 75
+if tempreture > 80:
+    print("its too hot!")
+    print("stay inside")
+else: 
+    print("enjoy outdoors!")
+
+tempreture = 50
+if tempreture > 80:
+    print("its too hot!")
+    print("stay inside")
+elif tempreture < 60:
+    print("its too cold")
+else: 
+    print("enjoy outdoors!")
