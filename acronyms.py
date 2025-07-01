9e6aa118bb969b6df82f73f3e645db1c8233d561
ShamserulAwsGeek <shamserul786@gmail.com>
2025-07-01 18:35:06 +0530

    class added

--- acronyms.py
+++ acronyms.py
@@ -2,18 +2,34 @@ def find_acronym():
     look_up = input('what software acronym would you like to look up?\n')
 
     found = False
-    with open('acronyms.txt') as file:
+    try:
+      with open('acronyms.txt') as file:
         for line in file:
             if look_up in line:
                 print(line)
                 found = True
                 break
+    except FileNotFoundError as e:
+       print('File not found')
+       return
+        
 
     if not found:
         print('The acronym does not exist')
 
-def aad_acronym():
+def add_acronym():
     acronym = input('what acronym do you want to add?\n')
     definition = input('what is the definition?\n')
     with open('acronyms.txt', 'a') as file:
      file.write(acronym + '-' + definition + '\n')
+
+
+def main():
+    #ask the user whether they want to find or add an acronym
+    choice = input('Do you want to find(F) or add(A) an acronym?')
+    if choice == 'F':
+        find_acronym()
+    elif choice == 'A':
+        add_acronym()
+
+main()
